f80f6d2a800383773b4239cf6ca3b7128452efb3
EndireKT <72508923+EndireKT@users.noreply.github.com>
2022-09-27 16:46:14 +0800

    greenworld & inverted color

--- js/kernel_projSubmission.js
+++ js/kernel_projSubmission.js
@@ -3,6 +3,7 @@ const canvasParent = document.getElementById('canvas-parent');
 const weirdFilter = document.getElementById('weird-filter');
 const gpuEnabled = document.getElementById('gpu-enabled');
 const fpsNumber = document.getElementById('fps-number');
+
 let lastCalledTime = Date.now();
 let fps;
 let delta;
@@ -11,51 +12,44 @@ gpuEnabled.onchange = () => {
   if (dispose) dispose();
   dispose = setup();
 };
+
+
 function setup() {
   let disposed = false;
+
   const gpu = new GPU({ mode: gpuEnabled.checked ? 'gpu' : 'cpu' });
 
-  // THIS IS THE IMPORTANT STUFF
-  const kernel = gpu.createKernel(function (frame, weirdFilter, k) {
-    const pixel = frame[this.thread.y][this.thread.x];
-    var col = [0, 0, 0];
-    if (weirdFilter) {
-      if (this.thread.y > 0 && this.thread.y < 768 - 2 && this.thread.x < 1024 - 2 && this.thread.x > 0) {
+  gpu
+    .addFunction(greenWorld)
+    .addFunction(invertedColor1);
 
-        const a0 = frame[this.thread.y + 1][this.thread.x - 1];
-        const a1 = frame[this.thread.y + 1][this.thread.x];
-        const a2 = frame[this.thread.y + 1][this.thread.x + 1];
-        const a3 = frame[this.thread.y][this.thread.x - 1];
-        const a4 = frame[this.thread.y][this.thread.x];
-        const a5 = frame[this.thread.y][this.thread.x + 1];
-        const a6 = frame[this.thread.y - 1][this.thread.x - 1];
-        const a7 = frame[this.thread.y - 1][this.thread.x];
-        const a8 = frame[this.thread.y - 1][this.thread.x + 1];
-        for (var i = 0; i < 3; i++) {       // Compute the convolution for each of red [0], green [1] and blue [2]
-          col[i] = a0[i] * k[0] + a1[i] * k[1] + a2[i] * k[2] + a3[i] * k[3] + a4[i] * k[4] + a5[i] * k[5] + a6[i] * k[6] + a7[i] * k[7] + a8[i] * k[8];
-        }
-        this.color(col[0], col[1], col[2], 1);
+  // THIS IS THE IMPORTANT STUFF
+  const kernel = gpu.createKernel(
+    function (frame, weirdFilter) {
+      const pixel = frame[this.thread.y][this.thread.x];
+      if (weirdFilter) {
+
+
+
       } else {
         this.color(pixel.r, pixel.g, pixel.b, pixel.a);
       }
-    } else {
-      this.color(pixel.r, pixel.g, pixel.b, pixel.a);
-    }
-  }, {
+    }, {
+    // LEAVE these
     output: [1024, 768],
     graphical: true,
     tactic: 'precision'
-  });
+  }
+  );
 
+  // DO NOT TOUCH AFTER THIS (for now...)
   canvasParent.appendChild(kernel.canvas);
   const videoElement = document.querySelector('video');
   function render() {
     if (disposed) {
       return;
     }
-    kernel(videoElement, weirdFilter.checked, [-1, -1, -1,
-    -1, 8, -1,
-    -1, -1, -1]);
+    kernel(videoElement, weirdFilter.checked);
     window.requestAnimationFrame(render);
     calcFPS();
   }
@@ -89,3 +83,36 @@ function calcFPS() {
   fpsNumber.innerHTML = fps.toFixed(0);
 }
 
+
+function greenWorld(r, g, b, a) {
+  return [0, g, 0, a];
+}
+
+function invertedColor1(r, g, b, a) {
+  return [1 - r, 1 - g, 1 - b, a];
+}
+
+
+
+// Green World Filter Code
+/*
+        var r = pixel[0];
+        var g = pixel[1];
+        var b = pixel[2];
+        var a = pixel[3];
+
+        const result = greenWorld(r, g, b, a);
+        this.color(result[0], result[1], result[2], result[3]);
+*/
+
+
+// Inverted Color 
+/*
+        var r = pixel[0];
+        var g = pixel[1];
+        var b = pixel[2];
+        var a = pixel[3];
+
+        const result = invertedColor1(r, g, b, a);
+        this.color(result[0], result[1], result[2], result[3]);
+*/
